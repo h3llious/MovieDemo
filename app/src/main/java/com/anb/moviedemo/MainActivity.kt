166a4a1067ba1ce0ab52ca672f48bb5f36cc1657
NhatB <nhat.bui@eastagile.com>
2022-12-29 11:40:51 +0700

    [c] Setup navigation library

--- app/src/main/java/com/anb/moviedemo/MainActivity.kt
+++ app/src/main/java/com/anb/moviedemo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anb.moviedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.anb.moviedemo.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
